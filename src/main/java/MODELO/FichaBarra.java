04773c14566a0f11dc218d5cac5cffc0fac03c5e
AnxoLeiroLoureiro <anxoleirol@gmail.com>
2023-02-02 08:47:15 +0100

    PROBANDO INTERFAZ

--- src/main/java/MODELO/FichaBarra.java
+++ src/main/java/MODELO/FichaBarra.java
@@ -16,14 +16,14 @@ public class FichaBarra extends Ficha {
     Iterator<Cadrado> it = cadrados.iterator();
 
     public FichaBarra() {
-       Cadrado cadrado0 = new Cadrado(120,0, Color.YELLOW);
-        Cadrado cadrado1 = new Cadrado(120,30, Color.YELLOW);
-        Cadrado cadrado2 = new Cadrado(150,30, Color.YELLOW);
-        Cadrado cadrado3 = new Cadrado(150,0, Color.YELLOW);
-        cadrados.add(cadrado0);
-        cadrados.add(cadrado1);
-        cadrados.add(cadrado2);
-        cadrados.add(cadrado3);
+        Cadrado c1 = new Cadrado(120,0, Color.YELLOW);
+        Cadrado c2 = new Cadrado(120,30, Color.YELLOW);
+        Cadrado c3 = new Cadrado(150,30, Color.YELLOW);
+        Cadrado c4 = new Cadrado(150,0, Color.YELLOW);
+        cadrados.add(c1);
+        cadrados.add(c2);
+        cadrados.add(c3);
+        cadrados.add(c4);
         }
     }
 
